f04833be301800323c5152787e31181522b85d17
heimsk <henriquemuller936@gmail.com>
2022-12-23 16:35:46 -0300

    Added client startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Setup done!");
+import Client from "./structures/Client";
+import "dotenv/config";
+
+(async () => {
+  const client = new Client(process.env.TOKEN || "", {
+    restMode: true,
+    intents: 131071,
+  });
+  await client.init();
+})();
